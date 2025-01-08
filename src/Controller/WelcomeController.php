fc8a0d7bd9978f7160b0d3919a585e2df69c708d
C4rpz <jules.grosjean@efrei.net>
2025-01-08 17:27:45 +0100

    firstpage

---
+++ src/Controller/WelcomeController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+#[Route('/welcome')]
+class WelcomeController extends AbstractController
+{
+    #[Route('/', name: 'app_welcome_index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('welcome/index_custom.html.twig');
+    }
+
+
+
+}
